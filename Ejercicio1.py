efe2cecbccd47ee16684951330a25ac51e635ccc
Ezecristian <cristianezequielcortavarria@gmail.com>
2024-08-22 12:55:46 -0300

    first commit

---
+++ Ejercicio1.py
@@ -0,0 +1,5 @@
+#1. Escribir un programa que pida al usuario su nombre y lo salude. 
+# Pista: van a necesitar la función input().
+
+nombre = input("ingrese su nombre: ")
+print(f"Hola o {nombre}")
